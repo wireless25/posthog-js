686c0fb2b784537cb6294d2eba3f183f8a8b7b8b
Stephan Simonett <wireless25@me.com>
2025-11-30 13:49:50 +0100

    chore: deploy to netlify edge

--- examples/example-nuxt/nuxt.config.ts
+++ examples/example-nuxt/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  preset: 'netlify_edge',
   modules: ['@posthog/nuxt'],
   compatibilityDate: '2025-11-03',
   posthogConfig: {
